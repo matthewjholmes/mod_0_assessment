f88723d54b23b12c6fa2655b2b4a2b5d8bc98ae9
matthewjholmes <>
2021-06-11 11:06:25 -0600

    Add DEVELOPER hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+DEVELOPER = {
+    :name => "Dan",
+    :job_title => "Software Developer",
+    :annual_salary => 76000,
+    :commute_minutes => 17,
+    :popular => true,
+    :indigestion => false,
+    :languages => ["ruby", "JS"],
+    :lunch_rotation => ["Jimmy Johns", "Chipotle", "Jersey Mike's"]
+  }
