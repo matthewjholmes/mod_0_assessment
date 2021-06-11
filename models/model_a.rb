d7496763a119b14dc12d777dc8533aa9be2ad3da
matthewjholmes <>
2021-06-11 11:00:40 -0600

    Add DINOSAUR hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+DINOSAUR = {
+    :name => "Allen",
+    :type => "T-Rex",
+    :weight_in_kg => 4000,
+    :daily_food_consumption_in_kg => 100,
+    :terrifying => true,
+    :constipated => false,
+    :foods => ["goats", "chickens", "cows"],
+    :activities => ["sleeping", "eating", "snoozing", "prowling"]
+  }
