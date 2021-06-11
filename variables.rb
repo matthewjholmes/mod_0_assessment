5c1d22eac990e4a81113ca3c9f36600486feafe3
matthewjholmes <>
2021-06-11 10:52:13 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "The Rock"
+is_entertaining = true
+movie = "The Secret Garden"
+num_4 = 129
+num_6 = 54
+product = num_4 * num_6
+favorite_foods = ["bananas", "ice cream", "CheezIt"]
+favorite_foods.shift
+movie.length
